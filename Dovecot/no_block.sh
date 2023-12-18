3aa6a855f0c752a0c921653c0ef11aa8294bda44
es1305 <71124766+es1305@users.noreply.github.com>
2023-12-18 09:03:27 +0700

    Update no_block.sh

--- Dovecot/no_block.sh
+++ Dovecot/no_block.sh
@@ -2,16 +2,35 @@
 
 input=spammers.txt
 
+# Colors
+NORMAL=''
+GREEN=''
+RED=''
+if tty -s; then
+  NORMAL="$(tput sgr0)"
+  GREEN=$(tput setaf 2)
+  RED="$(tput setaf 1)"
+fi
+
 if [[ -e $input ]]; then
   i=$(cat $input | grep "@" | sort | uniq | wc -l)
-  echo "Found $i spammer's addresses in file"
+
+  if [[ $i -eq 0 ]]; then
+    echo
+    echo "${GREEN}Found ${RED}$i ${GREEN}spammer's addresses in $input"
+    echo "Nothing to remove!${NORMAL}"
+    exit 0
+  else
+    echo
+    echo "${GREEN}Found ${RED}$i ${GREEN}spammer's addresses in $input"
+    echo "Please wait for remove...${NORMAL}"
+  fi
+
   cat $input | grep "@" | sort | uniq | while read addr; do
     doveadm expunge -A mailbox "*" FROM $addr
   done
-  :>$input
-  echo "Finished!"
-
 else
-  echo "File $input not found!"
+  echo
+  echo "${RED}ERROR! File $input is not found!${NORMAL}"
   exit 1
 fi
