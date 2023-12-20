defa44113526ead352fa1498f8d5fc120668de77
es1305 <71124766+es1305@users.noreply.github.com>
2023-12-20 14:39:40 +0700

    Update hfm-name.sh

--- Icecast/hfm-name.sh
+++ Icecast/hfm-name.sh
@@ -7,11 +7,15 @@
 ## Crontab:
 ## * * * * * /root/script/hfm-name.sh > /dev/null 2>&1
 
-ADMIN=LOGIN
-PASSWORD=PASSWORD
+ADMIN=admin
+PASSWORD=password
+NOSONG='Радио+«Heart+FM»+Барнаул+-+Тел.:+\(3852\)+55-10-59,+e-mail:+radio@heartfm.ru'
+MAYAK='Радио+«Маяк»+Барнаул+-+Телефон+рекламной+службы:+(3852)+68-50-10'
+RUSSIA='«Радио+России»+Алтай+-+Телефон+рекламной+службы:+(3852)+68-50-10'
+VESTI='Радио+«Вести+ФМ»+Барнаул+-+Телефон+рекламной+службы:+(3852)+68-50-10'
 
 if [ ! -f /mnt/rds/cur_playing.xml ]; then
-  mount /mnt/rds
+    mount /mnt/rds
 fi
 
 for i in {1..29}; do
@@ -23,7 +27,7 @@ for i in {1..29}; do
         /usr/bin/awk 'BEGIN{FS="<.?ARTIST>"}{print $2}')
 
     if [[ -z "$NAME" || -z "$ARTIST" ]]; then
-        SONG=Радио+«Heart+FM»+Барнаул+-+Тел.:+\(3852\)+55-10-59,+e-mail:+radio@heartfm.ru
+        SONG=$NOSONG
     else
         SONG=$(echo "$ARTIST - $NAME" | sed "s/&amp;//g" | sed "s/  */ /g" | sed "s/ /+/g")
     fi
@@ -36,10 +40,10 @@ for i in {1..29}; do
 done
 
 curl -u $ADMIN:$PASSWORD -s \
-    "http://127.0.0.1:8001/$ADMIN/metadata?mount=/mayak&mode=updinfo&song=Радио+«Маяк»+Барнаул+-+Телефон+рекламной+службы:+(3852)+68-50-10"
+    "http://127.0.0.1:8001/$ADMIN/metadata?mount=/mayak&mode=updinfo&song=$MAYAK"
 
 curl -u $ADMIN:$PASSWORD -s \
-    "http://127.0.0.1:8001/$ADMIN/metadata?mount=/russia&mode=updinfo&song=«Радио+России»+Алтай+-+Телефон+рекламной+службы:+(3852)+68-50-10"
+    "http://127.0.0.1:8001/$ADMIN/metadata?mount=/russia&mode=updinfo&song=$RUSSIA"
 
 curl -u $ADMIN:$PASSWORD -s \
-    "http://127.0.0.1:8001/$ADMIN/metadata?mount=/vesti&mode=updinfo&song=Радио+«Вести+ФМ»+Барнаул+-+Телефон+рекламной+службы:+(3852)+68-50-10"
+    "http://127.0.0.1:8001/$ADMIN/metadata?mount=/vesti&mode=updinfo&song=$VESTI"
