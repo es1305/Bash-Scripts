088cec8adee151a4e6ae2cb0378797c9562e85d6
Eugene Strekalov <71124766+es1305@users.noreply.github.com>
2024-11-13 13:05:59 +0700

    Update hfm-name.sh

--- Icecast/hfm-name.sh
+++ Icecast/hfm-name.sh
@@ -5,45 +5,36 @@
 #########################
 
 ## Crontab:
-## * * * * * /root/script/hfm-name.sh > /dev/null 2>&1
+## * * * * * /root/scripts/ice-meta.sh > /dev/null 2>&1
 
 ADMIN=admin
 PASSWORD=password
-NOSONG='Радио+«Heart+FM»+Барнаул+-+Тел.:+\(3852\)+55-10-59,+e-mail:+radio@heartfm.ru'
+HEARTFM='Радио+«Heart+FM»+Барнаул+-+Тел.:+(3852)+55-10-59,+e-mail:+radio@heartfm.ru'
 MAYAK='Радио+«Маяк»+Барнаул+-+Телефон+рекламной+службы:+(3852)+68-50-10'
 RUSSIA='«Радио+России»+Алтай+-+Телефон+рекламной+службы:+(3852)+68-50-10'
 VESTI='Радио+«Вести+ФМ»+Барнаул+-+Телефон+рекламной+службы:+(3852)+68-50-10'
 
-if [ ! -f /mnt/rds/cur_playing.xml ]; then
-    mount /mnt/rds
-fi
-
+# Process Heart FM
 for i in {1..29}; do
-
-    NAME=$(cat /mnt/rds/cur_playing.xml |
-        /usr/bin/awk 'BEGIN{FS="<.?NAME>"}{print $2}')
-
-    ARTIST=$(cat /mnt/rds/cur_playing.xml |
-        /usr/bin/awk 'BEGIN{FS="<.?ARTIST>"}{print $2}')
-
+    RDS=$(smbclient -d 0 -A /root/.smbcredentials //heart-air-01/xml -c 'get cur_playing.xml -')
+    NAME=$(awk -F'<.?NAME>' '{print $2}' <<< "$RDS")
+    ARTIST=$(awk -F'<.?ARTIST>' '{print $2}' <<< "$RDS")
     if [[ -z "$NAME" || -z "$ARTIST" ]]; then
-        SONG=$NOSONG
+        SONG=$HEARTFM
+    elif grep -Ei '\$|промо|джингл|новости|погода|блок|реклама' <<< "$ARTIST"; then
+        SONG=$HEARTFM
     else
-        SONG=$(echo "$ARTIST - $NAME" | sed "s/&amp;//g" | sed "s/  */ /g" | sed "s/ /+/g")
+        SONG=$(echo "$ARTIST - $NAME" |
+            sed "s/&amp;\|feat\./ft\./g" | sed "s/  */ /g" | sed "s/ /+/g")
     fi
-
     curl -u $ADMIN:$PASSWORD -s \
-        "http://127.0.0.1:8001/$ADMIN/metadata?mount=/heartfm&mode=updinfo&song=$SONG"
-
+        "http://127.0.0.1/$ADMIN/metadata?mount=/heartfm&mode=updinfo&song=$SONG"
     sleep 2
-
 done
 
-curl -u $ADMIN:$PASSWORD -s \
-    "http://127.0.0.1:8001/$ADMIN/metadata?mount=/mayak&mode=updinfo&song=$MAYAK"
-
-curl -u $ADMIN:$PASSWORD -s \
-    "http://127.0.0.1:8001/$ADMIN/metadata?mount=/russia&mode=updinfo&song=$RUSSIA"
-
-curl -u $ADMIN:$PASSWORD -s \
-    "http://127.0.0.1:8001/$ADMIN/metadata?mount=/vesti&mode=updinfo&song=$VESTI"
+# Process all others
+for STATION in mayak russia vesti; do
+    SONG_VAR=$(echo "${STATION^^}")
+    curl -u $ADMIN:$PASSWORD -s \
+        "http://127.0.0.1/$ADMIN/metadata?mount=/$STATION&mode=updinfo&song=${!SONG_VAR}"
+done
